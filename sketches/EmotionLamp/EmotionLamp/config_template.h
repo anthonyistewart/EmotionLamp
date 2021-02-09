5af5dd4d7e6dd40a5103e5a52825e0e23f416c3f
Anthony I. Stewart <anthony@stewart-technologies.com>
2021-02-08 22:09:11 -0500

    Initial commit

---
+++ sketches/EmotionLamp/EmotionLamp/config_template.h
@@ -0,0 +1,20 @@
+// Copy this file and rename it config.h, then fill in your credentials
+
+/******************************* WIFI **************************************/
+
+#define WIFI_SSID       ""
+#define WIFI_PASS       ""
+
+/******************************* MQTT **************************************/
+
+#define MQTT_BROKER     ""
+#define MQTT_PORT       1883
+#define MQTT_USERNAME   ""
+#define MQTT_PASSWORD   ""
+
+#define MQTT_TOPIC_IN   ""
+#define MQTT_TOPIC_OUT  ""
+
+/******************************* DEVICE **************************************/
+
+#define DEVICE_ID ""
